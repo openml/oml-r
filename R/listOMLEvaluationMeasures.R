58fb7fe8fbd2e6d9d116b391a09d3fe63ba23ba3
Patrick Schulze <pat.schulze@yahoo.de>
2019-04-21 21:37:45 +0200

    Added list functions (needed for testing listOMLDataSets.R)

---
+++ R/listOMLEvaluationMeasures.R
@@ -0,0 +1,23 @@
+.listOMLEvaluationMeasures = function(verbosity = NULL) {
+  content = doAPICall(
+    api.call = "json/evaluationmeasure/list", file = NULL,
+    verbosity = verbosity, method = "GET"
+  )
+  res = fromJSON(txt = content)
+  data.table(name = res$evaluation_measures$measures$measure)
+}
+
+#' @title List available OpenML evaluation measures.
+#'
+#' @description
+#' The names of all evaluation measures which are used in at least one run are returned
+#' in a \code{data.frame}.
+#'
+#' @template note_memoise
+#'
+#' @template arg_verbosity
+#' @return [\code{data.frame}].
+#' @family listing functions
+#' @export
+#' @example inst/examples/listOMLEvaluationMeasures.R
+listOMLEvaluationMeasures = memoise(.listOMLEvaluationMeasures)
